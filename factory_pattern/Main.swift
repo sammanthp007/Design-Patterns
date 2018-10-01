cb21d8bacd92a444ee455e3aa4e977b5277338e7
Samman Bikram Thapa <sthapa@dynamicsignal.com>
2018-10-01 16:46:19 -0700

    Implement the main method

--- factory_pattern/Main.swift
+++ factory_pattern/Main.swift
@@ -102,3 +102,26 @@ struct JobContracterFactory
         }
     }
 }
+
+func main ()
+{
+    var contacts = [Contact]()
+    contacts.append(Contact(name: "J Rob", email: "jrob@example.com", job: .iOS, desiredSal: 135000))
+    
+    contacts.append(Contact(name: "K Rock", email: "krock@example.com", job: .Android, desiredSal: 134000))
+    
+    contacts.append(Contact(name: "P Money", email: "pmoney@example.com", job: .Web, desiredSal: 110000))
+    
+    contacts.append(Contact(name: "S Sham", email: "ssham@example.com", job: .QA, desiredSal: 95000))
+    
+    contacts.append(Contact(name: "L Roger", email: "lroger@example.com", job: .Product_Owner, desiredSal: 165000))
+    
+    
+    for contact in contacts
+    {
+        let client = JobContacterFactory.getJobSeeker(contact: contact)
+        print (client.sendRequestResumeEmail())
+    }
+}
+
+main()
